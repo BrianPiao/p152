0b028c24456e62881f983ac3064a6f5697d2096a
BrianPiao <74799865+BrianPiao@users.noreply.github.com>
2022-12-06 16:54:01 -0900

    Add files via upload

---
+++ p152/Box.js
@@ -0,0 +1,63 @@
+// Registering components
+AFRAME.registerComponent("move", {
+  schema: {
+    moveY: { type: "number", default: 1 },
+  },
+
+  tick: function () {
+    var pos = this.el.getAttribute("position");
+    this.data.moveY = this.data.moveY + 0.01;
+
+    pos.y = this.data.moveY;
+
+    this.el.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
+  },
+});
+
+update:function() {
+  window.addEventListener("click", e => {
+    this.data.clickCounter = this.data.clickCounter +1;
+    if (this.data.clickCounter === 1) {
+      const rotation = {x:0 , y:20 , z:0};
+      this.el.setAttribute("rotation",rotation);
+      } else if(this.data.clickCounter === 2)
+      {
+        const rotation = {x:0,y:100,x:0}
+      }
+  })
+}
+
+AFRAME.registerComponent("camera-zoom-out", {
+  schema: {
+    moveZ: { type: "number", default: 10 },
+  },
+
+  tick: function () {
+    this.data.moveZ = this.data.moveZ + 0.01;
+
+    var pos = this.el.getAttribute("position");
+
+    pos.z = this.data.moveZ;
+
+    this.el.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
+  },
+});
+
+AFRAME.registerComponent("fall-down", {
+  schema: {
+    moveY: { type: "number", default: 0 },
+  },
+  tick: function () {
+
+    window.addEventListener("click", (e) => {   
+
+      this.data.moveY = this.data.moveY - 0.0001;
+
+    });
+    var pos = this.el.getAttribute("position");
+    pos.y = pos.y + this.data.moveY;
+    this.el.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
+  }
+});
+
+
